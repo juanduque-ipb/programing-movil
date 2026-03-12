9fded68df1ea7001fcfb70c9329fb37a85eddac1
juanduque-ipb <juan.duque@pascualbravo.edu.co>
2026-03-11 21:31:39 -0500

    Create main.dart

---
+++ Flutter-1/main.dart
@@ -0,0 +1,24 @@
+import 'package:card/screens/home.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // Application name
+      title: 'Flutter Hello World',
+      // Application theme data, you can set the colors for the application as
+      // you want
+      theme: ThemeData(
+        // useMaterial3: false,
+        primarySwatch: Colors.blue,
+      ),
+      // A widget which will be started on application startup
+      home: const Home(),
+    );
+  }
+}
